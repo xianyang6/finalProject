524550f0f508963588b5e21556d633313f880e21
chenxiaofeng <xc29@illinois.edu>
2022-01-05 11:21:00 -0600

    feat: new gpa

--- src/gpaScore.js
+++ src/gpaScore.js
@@ -4,6 +4,7 @@ export default function gpaScore(){
     return(
       <>
       <img src = "images/gpaImg.jpg" alt = "gpa" width = "100%"></img>
+      <img src = "images/gpa2.jpg" alt = "gpa" width = "100%"></img>
       </>
     );
 }
